f9a5211776693219bf457fa219e36c3ee193feaa
Denys Labunets <denis@labunets.com>
2017-12-18 15:32:15 +0200

    int math to float

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 // Created by Denys Labunets on 12/18/17.
 // Copypasted from C lang book by Brian Kernighan and Dennis Ritchie
 //
-// print temp table in fahr. and cels.
+// print temp table in fahr. and cels. with float arith.
 //
 
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
     
     lower = 0;      /* min temp */
@@ -17,8 +17,8 @@ int main() {
     
     fahr = lower;
     while ( fahr <= upper ) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d %6d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr += step;
     }
     
