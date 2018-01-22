6c9723c2340487143429bd962033407b6d6f3a57
Denys Labunets <denis@labunets.com>
2018-01-22 11:25:02 +0200

    Reverse chars in line

--- 1.09.exercise.1.19.c
+++ 1.09.exercise.1.19.c
@@ -1,3 +1,10 @@
+//
+// Created by Denys Labunets on 1/22/18
+// Copypasted from C lang book by Brian Kernighan and Dennis Ritchie
+//
+// Reverse chars in line
+//
+
 #include <stdio.h>
 
 #define MAXLINE 1000
